790be67e19efd5cadfdb183cc57cc83aa9e6265c
therezien titouan <titouan.therezien@edu.univ-fcomte.fr>
2024-05-24 13:42:55 +0200

    modification du publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
+const publicPath = process.env.NODE_ENV === 'production' ? '/Portfolios-Stage/' : '/'
 
 module.exports = defineConfig({
   transpileDependencies: true,
