f0ff310c5ae48c256dc6153f0b3e9c95b13a8197
therezien titouan <titouan.therezien@edu.univ-fcomte.fr>
2024-06-12 22:22:35 +0200

    bchdsjkvchsdjkq

--- src/views/StageView.vue
+++ src/views/StageView.vue
@@ -1,20 +1,19 @@
 <template>
-    <div class=" ">
+    <div>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
-        <div class="py-8 border-b mb-8">
+        <div class="py-8 border-b mb-8 ">
             <h1 class="text-2xl font-bold">L'augmentation des données d'images</h1>
         </div>
+        <div class="grid grid-cols-3 gap-8 ">
 
-        <div class="space-y-8">
-            <div class=" border rounded-lg shadow-md p-6 bg-white">
+            <div class="py-8 border rounded-lg shadow-md p-6 bg-white">
                 <h2 class="text-xl font-semibold mb-4">Qu'est-ce que l'augmentation des images :</h2>
-                <p>- L'augmentation des images, c'est simplement le fait d'obtenir de nouvelles images grâce à des
+                <p>L'augmentation des images, c'est simplement le fait d'obtenir de nouvelles images grâce à des
                     images existantes.</p>
             </div>
 
             <div class="py-8 border rounded-lg shadow-md p-6 bg-white">
                 <h2 class="text-xl font-semibold mb-4">Pourquoi augmenter les images :</h2>
-                <p>Pour plusieurs raisons :</p>
                 <ul class="list-disc list-inside ml-4">
                     <li>Pour augmenter la taille de notre dataset</li>
                     <li>Pour améliorer la performance de notre modèle</li>
@@ -30,14 +29,12 @@
 
                     <li>Rotation</li>
                     <li>Translation</li>
-
                     <li>Zoom</li>
-
                     <li>Flipping</li>
-
                     <li>Ajout de bruit</li>
                     <li>Changement de luminosité</li>
                     <li>Changement de contraste</li>
+ 
                 </ul>
                 <h3 class="text-lg font-semibold mt-4">Les méthodes avancées :</h3>
                 <ul class="list-disc list-inside ml-4 space-y-2">
@@ -48,10 +45,8 @@
             </div>
 
             <div class="py-8 border rounded-lg shadow-md p-6 bg-white">
-                <h2 class="text-xl font-semibold mb-4">Les solutions que j'ai mises en place :</h2>
-                <p>Tout d'abord, j'ai cherché une bibliothèque d'augmentation d'image efficace, rapide et simple
-                    d'utilisation.</p>
-                <p>J'ai découvert plusieurs bibliothèques existantes :</p>
+                <h2 class="text-xl font-bold mb-4">Bibliothèques d'Augmentation d'Image</h2>
+                <p class="font-semibold">Voici quelques-unes des bibliothèques les plus populaires :</p>
                 <ul class="list-disc list-inside ml-4 space-y-2">
                     <li>Augmentor</li>
                     <li>Albumentations</li>
@@ -59,14 +54,43 @@
                     <li>Transforms</li>
                     <li>Keras</li>
                 </ul>
-                <p>Mon choix a été principalement guidé par la simplification de l'utilisation de la bibliothèque.</p>
-                <p>Mes choix se sont portés sur Albumentations et Keras, pour deux raisons différentes et deux cas
-                    précis :</p>
+                <p>Pour simplifier l'utilisation, j'ai choisi deux bibliothèques en particulier, chacune adaptée à un
+                    cas d'usage spécifique.</p>
+                <h2 class="pt-6 text-xl font-semibold mb-4">Mes choix se sont portés sur Albumentations et Keras, pour
+                    deux raisons différentes et deux cas
+                    précis :</h2>
+                <p>Albumentations est reconnue pour sa flexibilité et sa vitesse. Elle propose une large gamme de
+                    transformations qui peuvent être facilement appliquées à un dataset entier. Voici pourquoi
+                    Albumentations est un excellent choix pour l'augmentation des images de votre dataset :</p>
+
                 <ul class="list-disc list-inside ml-4 space-y-2">
-                    <li>Albumentations : pour l'augmentation des images de mon dataset</li>
-                    <li>Keras : pour l'augmentation des images en temps réel</li>
+                    <li><b>Richesse des transformations :</b> Rotation, découpage, ajustement de la luminosité et du
+                        contraste, flou, etc.</li>
+                    <li><b>Performance :</b> Conçue pour être rapide et efficace, même avec des datasets volumineux.
+                    </li>
+                    <li><b>Simplicité :</b> Facile à intégrer dans des pipelines de pré-traitement existants.</li>
                 </ul>
+
+                <h2 class="pt-6 text-xl font-semibold mb-4">Keras : Pour l'Augmentation des Images en Temps Réel
+                </h2>
+                <p>Keras, avec son API intuitive, offre des fonctionnalités d'augmentation d'image en temps réel, idéal
+                    pour le traitement des images lors de l'entraînement des modèles. Les avantages de Keras incluent :
+                </p>
+                <ul class="list-disc list-inside ml-4 space-y-2">
+                    <li><b>Intégration fluide :</b> S'intègre parfaitement avec les modèles Keras pour augmenter les
+                        images à la volée pendant l'entraînement.</li>
+                    <li><b>Transformations variées :</b> Inclut des transformations communes comme la rotation, le zoom,
+                        le retournement, etc.</li>
+                    <li><b>Facilité d'utilisation :</b> Grâce à son API conviviale, vous pouvez rapidement mettre en
+                        place l'augmentation d'image dans votre workflow d'entraînement.</li>
+
+                </ul>
+
+
+
+
             </div>
+
         </div>
     </div>
 </template>
@@ -83,4 +107,5 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
