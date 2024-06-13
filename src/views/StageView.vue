df9914c07468b51382250e3cfe547cec095cdaa8
therezien titouan <titouan.therezien@edu.univ-fcomte.fr>
2024-06-13 02:08:40 +0200

    bchdsjkvchsdjkq

--- src/views/StageView.vue
+++ src/views/StageView.vue
@@ -4,7 +4,7 @@
         <div class="py-8 border-b mb-8 ">
             <h1 class="text-2xl font-bold">L'augmentation des données d'images</h1>
         </div>
-        <div class="grid grid-cols-3 gap-8 ">
+        <div class="grid grid-cols-2 gap-4 grid-flow">
 
             <div class="py-8 border rounded-lg shadow-md p-6 bg-white">
                 <h2 class="text-xl font-semibold mb-4">Qu'est-ce que l'augmentation des images :</h2>
@@ -34,7 +34,7 @@
                     <li>Ajout de bruit</li>
                     <li>Changement de luminosité</li>
                     <li>Changement de contraste</li>
- 
+
                 </ul>
                 <h3 class="text-lg font-semibold mt-4">Les méthodes avancées :</h3>
                 <ul class="list-disc list-inside ml-4 space-y-2">
@@ -44,10 +44,17 @@
                 </ul>
             </div>
 
+            <div class=" grid grid-cols-2 py-8 border rounded-lg shadow-md p-6 bg-white">
+                <img src="@/assets/CatRotation.gif" alt="">
+                <img src="@/assets/CatZoom.gif" alt="">
+                <img src="@/assets/Normaltoflip.png" alt="">
+                <img src="@/assets/NormaltoNoise.png" alt="">
+            </div>
+
             <div class="py-8 border rounded-lg shadow-md p-6 bg-white">
                 <h2 class="text-xl font-bold mb-4">Bibliothèques d'Augmentation d'Image</h2>
                 <p class="font-semibold">Voici quelques-unes des bibliothèques les plus populaires :</p>
-                <ul class="list-disc list-inside ml-4 space-y-2">
+                <ul class="py-2 list-disc list-inside ml-4 ">
                     <li>Augmentor</li>
                     <li>Albumentations</li>
                     <li>ImgAug</li>
@@ -63,7 +70,7 @@
                     transformations qui peuvent être facilement appliquées à un dataset entier. Voici pourquoi
                     Albumentations est un excellent choix pour l'augmentation des images de votre dataset :</p>
 
-                <ul class="list-disc list-inside ml-4 space-y-2">
+                <ul class="py-2 list-disc list-inside ml-4 ">
                     <li><b>Richesse des transformations :</b> Rotation, découpage, ajustement de la luminosité et du
                         contraste, flou, etc.</li>
                     <li><b>Performance :</b> Conçue pour être rapide et efficace, même avec des datasets volumineux.
@@ -76,7 +83,7 @@
                 <p>Keras, avec son API intuitive, offre des fonctionnalités d'augmentation d'image en temps réel, idéal
                     pour le traitement des images lors de l'entraînement des modèles. Les avantages de Keras incluent :
                 </p>
-                <ul class="list-disc list-inside ml-4 space-y-2">
+                <ul class="py-2 list-disc list-inside ml-4 ">
                     <li><b>Intégration fluide :</b> S'intègre parfaitement avec les modèles Keras pour augmenter les
                         images à la volée pendant l'entraînement.</li>
                     <li><b>Transformations variées :</b> Inclut des transformations communes comme la rotation, le zoom,
@@ -90,6 +97,13 @@
 
 
             </div>
+            <div class="  py-8 border rounded-lg shadow-md p-6 bg-white">
+                <img src="@/assets/DATDAP.png" class="w-auto" alt="">
+
+            </div>
+
+            
+
 
         </div>
     </div>
@@ -107,5 +121,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
